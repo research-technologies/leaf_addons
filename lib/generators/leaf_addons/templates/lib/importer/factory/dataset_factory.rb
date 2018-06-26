99e15a0befc4cdab3dc378714abc3d31028b5b1f
Julie Allinson <julie.allinson@london.ac.uk>
2018-06-26 11:15:30 +0100

    extend find_model method in eprints importer to support common types and add corresponding factories

---
+++ lib/generators/leaf_addons/templates/lib/importer/factory/dataset_factory.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Importer
+  module Factory
+    class DatasetFactory < BaseFactory
+      self.klass = Dataset
+    end
+  end
+end
---
+++ lib/generators/leaf_addons/templates/lib/importer/factory/thesis_factory.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Importer
+  module Factory
+    class ThesisFactory < BaseFactory
+      self.klass = Thesis
+    end
+  end
+end
