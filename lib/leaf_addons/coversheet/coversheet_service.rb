21fe55ba31d1815de4ee5157077ebf6b2747324c
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2019-10-21 23:52:53 +0000

    remove ref to file in case the file is naff

--- lib/leaf_addons/coversheet/coversheet_service.rb
+++ lib/leaf_addons/coversheet/coversheet_service.rb
@@ -40,9 +40,9 @@ module LeafAddons
                                         margin: LeafAddons.config.coversheet_margin)
       image
       #Load a ttf file to match the font from the config (DB a ttf file with a matching name will need to be put in place)
-      font_file=Rails.root.join('app/assets/fonts/'+LeafAddons.config.coversheet_font+'.ttf')
-      Rails.logger.error("font file : #{font_file}")
-      coversheet.font_families.update(LeafAddons.config.coversheet_font =>{:normal=>font_file})
+   #   font_file=Rails.root.join('app/assets/fonts/'+LeafAddons.config.coversheet_font+'.ttf')
+   #   Rails.logger.error("font file : #{font_file}")
+   #   coversheet.font_families.update(LeafAddons.config.coversheet_font =>{:normal=>font_file})
       coversheet.font LeafAddons.config.coversheet_font
       coversheet.font_size LeafAddons.config.coversheet_fontsize_small
       Rails.logger.error("font : #{LeafAddons.config.coversheet_font}")
